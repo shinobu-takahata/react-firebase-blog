0c8b7ef4aa4c4d9cecdfc98de6b345a14769a711
shinobu_takahata <shinoshino2335@gmail.com>
2023-07-28 10:20:07 +0900

    ホーム画面　デザイン調整

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const posts = await getDocs(collection(db, "posts"));
+      console.log(posts.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(posts.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+  return (
+    <div className="home-page">
+      {postList.map((post) => {
+        return (
+          <div className="post-contents" key={post.id}>
+            <div className="post-header">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="post-text-container">{post.postsText}</div>
+            <div className="name-and-delete-button">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Home;
